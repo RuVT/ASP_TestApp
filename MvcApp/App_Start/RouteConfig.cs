25174930abd02b581f5a3c4629f6cb9d82c4f29a
Rubén Vazquez <vazquezt101@gmail.com>
2017-04-20 17:27:15 -0700

    Playing around with the MVC like RoR code style, a controller was added with two views: index and welcome

--- MvcApp/App_Start/RouteConfig.cs
+++ MvcApp/App_Start/RouteConfig.cs
@@ -18,6 +18,12 @@ namespace MvcApp
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+                name: "HelloHello",
+                url: "{controller}/{action}/{name}/{number}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
         }
     }
 }
---
+++ MvcApp/Controllers/HelloHello.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApp.Controllers
+{
+    public class TestModel
+    {
+        public int Number { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class HelloHelloController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [AcceptVerbs(HttpVerbs.Get)]
+        public ActionResult Welcome(TestModel model)
+        {
+            ViewBag.data = model;
+            return View();
+        }
+
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+    }
+}
+
