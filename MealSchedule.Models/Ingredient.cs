67a166ed8ce03564705c4a797bac3faf1009074e
Rubén Vazquez <vazquezt101@gmail.com>
2017-04-20 18:02:36 -0700

    Adding some Models for interact with the DB

---
+++ MealSchedule.Models/Ingredient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace MealSchedule.Models
+{
+    public partial class Ingredient
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public List<string> SimilarNames { get; set; }
+    }
+}
---
+++ MvcApp/Models/Ingredient.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+using MealSchedule.Models;
+
+namespace MvcApp.Models
+{
+	public class IngredientDBContext: DbContext
+	{
+        public DbSet<Ingredient> Ingredients { get; set; }
+	}
+}
